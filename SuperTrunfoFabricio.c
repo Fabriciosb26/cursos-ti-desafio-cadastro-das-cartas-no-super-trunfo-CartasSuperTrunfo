c91159bf8fff9cd4edfc609abe7b3842b9298208
Fabriciosb26 <fabriciosilvabritto389@gmail.com>
2025-03-10 22:32:21 +0000

    Concluido

---
+++ SuperTrunfoFabricio.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+int main(){
+    char estado1, estado2;
+    char codcarta1[50], codcarta2[50];
+    char cidade1[50], cidade2[50];
+    int populacao1, populacao2, pontos1, pontos2;
+    float area1, area2, pib1, pib2;
+
+        printf("Digite seu primeiro estado \n");
+        scanf("%s", &estado1);
+
+        printf("Digite o codigo da cidade \n");
+        scanf("%s", &codcarta1);
+
+        printf("Digite o nome da cidade \n");
+        scanf("%s", &cidade1);
+
+        printf("Digite a quantidade de habitantes \n");
+        scanf("%s", &populacao1);
+
+        printf("Digite a área km² da cidade \n");
+        scanf("%f", &area1);
+
+        printf("Digite o PIB da cidade \n");
+        scanf("%f", &pib1);
+
+        printf("Digite a quantidade de pontos turísticos da cidade \n");
+        scanf("%d", &pontos1);
+
+        printf("Digite seu segundo estado \n");
+        scanf("%s", &estado2);
+
+        printf("Digite o codigo da cidade \n");
+        scanf("%s", &codcarta2);
+
+        printf("Digite o nome da cidade \n");
+        scanf("%s", &cidade2);
+
+        printf("Digite a quantidade de habitantes \n");
+        scanf("%s", &populacao2);
+
+        printf("Digite a área km² da cidade \n");
+        scanf("%f", &area2);
+
+        printf("Digite o PIB da cidade \n");
+        scanf("%f", &pib2);
+
+        printf("Digite a quantidade de pontos turísticos da cidade \n");
+        scanf("%d", &pontos2);
+
+        return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
